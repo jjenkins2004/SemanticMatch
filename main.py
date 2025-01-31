a5d38210b33c3a737a98f4021371ef9ee4f12934
Joshua Jenkins <2004joshuaj@gmail.com>
2025-01-30 17:22:09 -0800

    translation from object url (broken)

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 from transcriber import getTranscript
 from semantic import find_similarity
 
-audioFile = "file/test.webm"
-referenceSentence = "Tom and his brothers like to eat all the candy."
-#en for english zh for chinese
-result = getTranscript(audioFile, "en")
-print("transcribed: " + result)
-similarity = find_similarity(result, referenceSentence)
-print("cosine similarity: " + str(similarity))
-if (similarity < 0.6) :
-    print("Answer is completely different")
-elif (similarity < 0.75) :
-    print("Answer is somewhat related")
-elif (similarity < 0.875) :
-    print("Answer is similar")
-elif (similarity <= 1) :
-    print("Answer is very similar")
+def main() :
+    fileLink = "https://merls-audio.s3.us-east-2.amazonaws.com/11-27-2024/user_rebecca/question_1_1732680999927.webm"
+    referenceSentence = "Tom and his brothers like to eat all the candy."
+    #en for english zh for chinese
+    result = getTranscript(fileLink, "en")
+    if (result == "") :
+        print("error in transcribing")
+        return
 
+    print("transcribed: " + result)
+    similarity = find_similarity(result, referenceSentence)
+    print("cosine similarity: " + str(similarity))
+    if (similarity < 0.6) :
+        print("Answer is completely different")
+    elif (similarity < 0.75) :
+        print("Answer is somewhat related")
+    elif (similarity < 0.875) :
+        print("Answer is similar")
+    elif (similarity <= 1) :
+        print("Answer is very similar")
+
+if __name__ == "__main__":
+    main()
--- transcriber.py
+++ transcriber.py
@@ -1,16 +1,32 @@
 import whisper
+import requests
+import io
 
-def getTranscript(fileName, language):
+def getTranscript(fileLink, language):
+
+    #get audio data from object link
+    response = requests.get(fileLink)
+
+    if (response.status_code != 200) :
+        return ""
+    
+    #get the audio byte stream
+    buffer = io.BytesIO(response.content)
+    buffer.name = "file.webm"
 
     #load the model
     model = whisper.load_model("small")
 
-    #format the audio
-    audio = whisper.load_audio(fileName)
-    audio = whisper.pad_or_trim(audio)
+    try :
+        #load and format the audio
+        audio = whisper.load_audio(buffer)
+        audio = whisper.pad_or_trim(audio)
 
-    # make log-Mel spectrogram and move to the same device as the model (more formatting basically)
-    mel = whisper.log_mel_spectrogram(audio, n_mels=model.dims.n_mels).to(model.device)
+        # make log-Mel spectrogram and move to the same device as the model (more formatting basically)
+        mel = whisper.log_mel_spectrogram(audio, n_mels=model.dims.n_mels).to(model.device)
+    except Exception as e:
+        print(f"could not load audio data from {fileLink}: {e}")
+        return ""
 
     #set the correct language
     options = whisper.DecodingOptions(language=language)
